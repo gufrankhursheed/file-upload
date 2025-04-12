719887b62f9855295a2ed2164027d88bf2729e71
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-04-12 12:02:20 +0530

    add multer middleware

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
-export default app
+app.get("/", (req, res) => {
+  res.send(`
+        <h1>Upload a File</h1>
+        <form method="POST" action="/upload" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <button type="submit">Upload</button>
+        </form>
+        `);
+});
+
+export default app;
