1fa646652b9ca335102c6764ff3ab5bbbfebc4a6
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-04-12 12:23:48 +0530

    server setup with http

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv"
+import http from "http"
 import app from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
 
-app.listen(process.env.PORT || 500, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+const server = http.createServer()
+
+server.listen(process.env.PORT2 || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT2}`)
+})
+
+app.listen(process.env.PORT1 || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT1}`)
 })
